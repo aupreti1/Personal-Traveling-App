e3fcbb278a2d5b0541cbed719cb70c4d7d548891
Gigi Tannous <gtannous16@gmail.com>
2019-04-18 22:53:08 -0400

    added questions back and removed datajsfile

--- javascript/logic.js
+++ javascript/logic.js
@@ -114,6 +114,157 @@ $(document).ready(function () {
             "./assets/images/julieandjulia.jpg"]
 
     }, {
+        question: "What show would you prefer to watch?",
+        img: ["./assets/images/penny.jpg",
+            "./assets/images/darktourist.jpg",
+            "./assets/images/blueplanet.jpg",
+            "./assets/images/reign.jpg",
+            "./assets/images/jerseyshore.jpg",
+            "./assets/images/parksandrec.jpg",
+            "./assets/images/cupcake.jpg"]
+    
+    }, {
+        question: "What would be your dream career?",
+        text: ["Medical Lab Technician",
+            "Flight Attendant",
+            "Marine Biologist",
+            "Archeologist",
+            "Event Planner",
+            "Doggie Daycare Owner",
+            "Recipe Tester"]
+    
+    }, {
+        question: "What music genre do you prefer?",
+        text: ["Rock and Roll",
+            "Pop Music", "Folk Music",
+            "Country Music", "House Music",
+            "Classical Music", "Punk Rock"]
+    
+    }, {
+        question: "What could you NOT live without?",
+        text: ["Netflix/Hulu",
+            "Cell Phone", "Favorite Sneakers",
+            "Favorite Book", "Booze", "Pet",
+            "Favorite Food"]
+    
+    }, {
+        question: "What is your preferred way of communication?",
+        text: ["None", "FaceTime",
+            "Face to Face", "Letters",
+            "Snapchat", "Text", "Instagram"]
+    
+    }, {
+        question: "Which of these do you find most satisfying?",
+        text: ["Spending time on your own",
+            "Seeing new places", "Hiking",
+            "Reading about different times and places",
+            "Hitting up alocal club or pub",
+            "Laying on a beach or in a hammock",
+            "Discovering new places to eat"]
+    
+    }, {
+        question: "Which of these superpowers sound more appealing?",
+        img: ["./assets/images/weather.jpg",
+            "./assets/images/superman.jpg",
+            "./assets/images/fire.jpg",
+            "./assets/images/speed.jpg",
+            "./assets/images/mindreading.jpg",
+            "./assets/images/telekinesis.jpg",
+            "./assets/images/plantbased.jpg"]
+    
+    }, {
+        question: "Which of these could be your spirit animals?",
+        img: ["./assets/images/cat.jpg",
+            "./assets/images/owl.jpg",
+            "./assets/images/horse.jpg",
+            "./assets/images/dino.jpg",
+            "./assets/images/monkey.jpg",
+            "./assets/images/dog.jpg",
+            "./assets/images/bear.jpg"]
+    
+    }, {
+        question: "Which of these hobbies interest you the most?",
+        text: ["Coloring Books",
+            "Learning a language", "Camping",
+            "Reading", "Dancing", "Yoga",
+            "Cooking"]
+    
+    }, {
+        question: "Which of these qualities is most appealing in a partner/friend",
+        text: ["Loyalty", "Adventurous",
+            "Patient", "Intelligent", "Fun",
+            "Laid Back", "Open Minded"]
+    
+    }, {
+        question: "Which of these celebs would you want to meet?",
+        img: ["./assets/images/johnnydepp.jpg",
+            "./assets/images/angelinajolie.jpg",
+            "./assets/images/zacefron.jpg",
+            "./assets/images/georgeclooney.jpg",
+            "./assets/images/leonardodicaprio.jpg",
+            "./assets/images/sandrabullock.jpg",
+            "./assets/images/chrissyteigen.jpg"]
+    
+    }, {
+        question: "What is your ideal date?",
+        text: ["No date for me! I'd rather hang with my fur child",
+            "Going for a tour around the city",
+            "Hiking a mountain",
+            "Going to a museum or art gallery",
+            "Going to a night club",
+            "Dinner and a movie",
+            "Going to a high-end restaurant with a 7-course meal"]
+    }, {
+        question: "What picture most constitutes as your dream house?",
+        img: ["./assets/images/secludedhouse.jpg",
+            "./assets/images/touristichouse.jpg",
+            "./assets/images/outdoorshouse.jpg",
+            "./assets/images/historicalhouse.jpg",
+            "./assets/images/partyhouse.jpg",
+            "./assets/images/relaxinghouse.jpg",
+            "./assets/images/foodiehouse.jpg"]
+    
+    }, {
+        question: "Which would you rather have?",
+        img: ["./assets/images/benandjerrys.jpg",
+            "./assets/images/macaroons.jpg",
+            "./assets/images/foodinapot.jpg",
+            "./assets/images/wineandcheese.jpg",
+            "./assets/images/drinks.jpg",
+            "./assets/images/hamburgerandfries.jpg",
+            "./assets/images/fancyfoods.jpg"]
+    
+    }, {
+        question: "Which of these annoys you the most?",
+        text: ["People who are mean to others",
+            "Being bored with nothing to do",
+            "People who constantly complain",
+            "Unnecessary drama",
+            "Phone battery's dead and you don't have a charger",
+            "Feeling stressed and overwhelmed",
+            "Messy House"]
+    
+    }, {
+        question: "What vehicle would you rather drive?",
+        img: ["./assets/images/truck.jpg",
+            "./assets/images/mustang.jpg",
+            "./assets/images/jeep.jpg",
+            "./assets/images/vintagecar.jpg",
+            "./assets/images/audi.jpg",
+            "./assets/images/rangerover.jpg",
+            "./assets/images/foodtruck.jpg"]
+    }, {
+        question: "How would you rather listen to your music?",
+        text: ["Headphones", "Blasting in the Car",
+            "Music Festival", "Vinyl", "Party Inside a Club",
+            "Live Band at a Local Joint", "Bluetooth Speaker"]
+    }, {
+        question: "You won the lottery! What is the first thing you do?",
+        text: ["Move off the grid", "Travel the World",
+            "Buy an Airstream and Travel", "Buy a Historical Castle",
+            "Throw a Wild Party", "Quit Your Job and Do as You Please",
+            "Treat Yourelf to an Expensive Meal"]
+    },  {
         question: "What shoes would you rather wear any day/all day?",
         img: ["./assets/images/bedslippers.jpg",
             "./assets/images/chacos.jpg",
